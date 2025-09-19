04977768484f4d2b29519d7ec6a300e5f90a568c
Emoral12 <eugbermorales77@gmail.com>
2025-09-19 18:21:45 -0400

    More progress
    had to restart UnrealEditor

--- Source/In_Lab_Assignment/ScoreGameInstance.cpp
+++ Source/In_Lab_Assignment/ScoreGameInstance.cpp
@@ -1,5 +1,24 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+// GameInstance .cpp file
 
 #include "ScoreGameInstance.h"
 
+// this is not as annotated cuz what am i even meant to annotate? the cpp file will simply do what the header allows it to do
+// look at the other file for annotations that add depth to this simple task
+
+void UScoreGameInstance::AddScore(int32 Points) 
+{
+	TotalScore += Points;
+
+	// debug text to see if function is even going through or not
+	UE_LOG(LogTemp, Log, TEXT("You got a gem! Your Score is now: %d"), TotalScore);
+}
+
+int32 UScoreGameInstance::GetScore() const
+{
+	return TotalScore;
+}
+
+void UScoreGameInstance::ResetScore()
+{
+	TotalScore = 0;
+}
--- Source/In_Lab_Assignment/ScoreGameInstance.h
+++ Source/In_Lab_Assignment/ScoreGameInstance.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// The header file for my Singleton. Also, why does Unreal always want a copyright thingy here? Gives me an error when I delete it.
 
 #pragma once
 
@@ -13,5 +13,22 @@ UCLASS()
 class IN_LAB_ASSIGNMENT_API UScoreGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
+
+public:
+	// this here adds points to a player score
+	UFUNCTION(BlueprintCallable, Category = "Score")
+	void AddScore(int32 Points);
+
+	// gets the scroe, so updates it pretty much
+	UFUNCTION(BlueprintCallable, Category = "Score")
+	int32 GetScore() const;
+
+	// resets score, in case of new level but currently not used 
+	UFUNCTION(BlueprintCallable, Category = "Score")
+	void ResetScore();
+
+private:
+	// Starting score is set at 0 (duh)
+	int32 TotalScore = 0;
 	
 };
